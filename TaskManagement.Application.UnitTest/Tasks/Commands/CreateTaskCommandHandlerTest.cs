7c46d9773ba856c4fe522a84b247e868cefecbfb
birehan <birehananteneh4@gmail.com>
2023-05-19 15:36:34 +0300

    feat(backend-birehan): update test based on changes

--- TaskManagement.Application.UnitTest/Tasks/Commands/CreateTaskCommandHandlerTest.cs
+++ TaskManagement.Application.UnitTest/Tasks/Commands/CreateTaskCommandHandlerTest.cs
@@ -1,101 +1,108 @@
 using AutoMapper;
+using FluentValidation;
+using Moq;
+using Shouldly;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using TaskManagement.Application.Contracts.Persistence;
 using TaskManagement.Application.Features.Tasks.CQRS.Commands;
 using TaskManagement.Application.Features.Tasks.CQRS.Handlers;
 using TaskManagement.Application.Features.Tasks.DTOs;
-using TaskManagement.Application.Profiles;
+using TaskManagement.Application.Interfaces;
 using TaskManagement.Application.Responses;
 using TaskManagement.Application.UnitTest.Mocks;
-
-using MediatR;
-using Moq;
-using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xunit;
+using TaskManagement.Application.Profiles;
 
 namespace TaskManagement.Application.UnitTest.Tasks.Commands
 {
     public class CreateTaskCommandHandlerTest
     {
-
         private readonly IMapper _mapper;
         private readonly Mock<IUnitOfWork> _mockRepo;
-        private readonly CreateTaskDto _TaskDto;
+        private readonly Mock<IUserAccessor> _mockUserAccessor;
+        private readonly CreateTaskDto _taskDto;
         private readonly CreateTaskCommandHandler _handler;
+
         public CreateTaskCommandHandlerTest()
         {
             _mockRepo = MockUnitOfWork.GetUnitOfWork();
+            _mockUserAccessor = new Mock<IUserAccessor>();
+
             var mapperConfig = new MapperConfiguration(c =>
             {
                 c.AddProfile<MappingProfile>();
             });
             _mapper = mapperConfig.CreateMapper();
 
-            _TaskDto = new CreateTaskDto
+            _taskDto = new CreateTaskDto
             {
                 Title = "title",
                 Description = "Sample Content",
                 Status = true,
                 StartDate = DateTime.Now.Date,
                 EndDate = DateTime.Now.Date,
-        };
+            };
 
-            _handler = new CreateTaskCommandHandler(_mockRepo.Object, _mapper);
-
+            _handler = new CreateTaskCommandHandler(_mockRepo.Object, _mapper, _mockUserAccessor.Object);
         }
 
-
         [Fact]
-        public async Task CreateTask()
+        public async Task CreateTask_ValidData_ReturnsSuccessResult()
         {
-            var result = await _handler.Handle(new CreateTaskCommand() { TaskDto = _TaskDto }, CancellationToken.None);
+            // Arrange
+            _mockUserAccessor.Setup(u => u.GetCurrentUser()).ReturnsAsync(new Domain.AppUser());
+
+            // Act
+            var result = await _handler.Handle(new CreateTaskCommand { TaskDto = _taskDto }, CancellationToken.None);
+
+            // Assert
             result.ShouldBeOfType<Result<int>>();
             result.Success.ShouldBeTrue();
-            var Tasks = await _mockRepo.Object.TaskRepository.GetAll();
-            Tasks.Count.ShouldBe(3);
 
+            var tasks = await _mockRepo.Object.TaskRepository.GetAll();
+            tasks.Count.ShouldBe(3);
         }
 
         [Fact]
         public async Task CreateTask_InvalidStartDate_FailsValidation()
         {
             // Arrange
-            _TaskDto.StartDate = DateTime.Now.Date.AddDays(-1); // Set an invalid start date (before the current date)
+            _mockUserAccessor.Setup(u => u.GetCurrentUser()).ReturnsAsync(new Domain.AppUser());
+            _taskDto.StartDate = DateTime.Now.Date.AddDays(-1); // Set an invalid start date (before the current date)
 
             // Act
-            var result = await _handler.Handle(new CreateTaskCommand() { TaskDto = _TaskDto }, CancellationToken.None);
+            var result = await _handler.Handle(new CreateTaskCommand { TaskDto = _taskDto }, CancellationToken.None);
 
             // Assert
             result.ShouldBeOfType<Result<int>>();
             result.Success.ShouldBeFalse();
             result.Errors.ShouldContain("Start Date must be greater than or equal to the current date.");
+
             var tasks = await _mockRepo.Object.TaskRepository.GetAll();
             tasks.Count.ShouldBe(2); // The task should not be created
         }
-
 
         [Fact]
         public async Task CreateTask_InvalidEndDate_FailsValidation()
         {
             // Arrange
-            _TaskDto.EndDate = DateTime.Now.Date.AddDays(-1); // Set an invalid end date (before the start date)
+            _mockUserAccessor.Setup(u => u.GetCurrentUser()).ReturnsAsync(new Domain.AppUser());
+            _taskDto.EndDate = DateTime.Now.Date.AddDays(-1); // Set an invalid end date (before the start date)
 
             // Act
-            var result = await _handler.Handle(new CreateTaskCommand() { TaskDto = _TaskDto }, CancellationToken.None);
+            var result = await _handler.Handle(new CreateTaskCommand { TaskDto = _taskDto }, CancellationToken.None);
 
             // Assert
             result.ShouldBeOfType<Result<int>>();
             result.Success.ShouldBeFalse();
             result.Errors.ShouldContain("End Date must be greater than or equal to the Start Date.");
+
             var tasks = await _mockRepo.Object.TaskRepository.GetAll();
             tasks.Count.ShouldBe(2); // The task should not be created
         }
-       
     }
+
 }
